2670510e2aa66b6667fe0036eff3e9ec93e52d9d
Richard Russon <rich@flatcap.org>
2017-07-22 14:45:11 +0100

    set/get methods

--- hcache.c
+++ hcache.c
@@ -1,37 +1,80 @@
+#include <string.h>
 #include "hcache.h"
+#include "buffer.h"
 #include "config_set.h"
+#include "lib.h"
 #include "mutt_options.h"
 
-static bool string_set(struct HashElem *e, const char *value, struct Buffer *err)
+const char *hcache_backends[] = {
+  "tokyocabinet", "kyotocabinet", "qdbm", "gdbm", "bdb", "lmdb",
+};
+
+
+static bool hc_string_set(struct HashElem *e, const char *value, struct Buffer *err)
 {
-  // Set a hcache by string
+  if (DTYPE(e->type) != DT_HCACHE)
+  {
+    mutt_buffer_printf(err, "Variable is not an hcache");
+    return false;
+  }
+
+  intptr_t index = -1;
+
+  for (int i = 0; i < mutt_array_size(hcache_backends); i++)
+  {
+    if (strcmp(value, hcache_backends[i]) == 0)
+    {
+      index = i;
+      break;
+    }
+  }
+
+  if (index < 0)
+  {
+    mutt_buffer_printf(err, "Not a valid hcache backend: %s", value);
+    return false;
+  }
+
+  e->data = (void *) index;
   return true;
 }
 
-static const char *string_get(struct HashElem *e, struct Buffer *err)
+static const char *hc_string_get(struct HashElem *e, struct Buffer *err)
 {
-  // get a hcache as a string
-  return "lmdb";
+  if (DTYPE(e->type) != DT_HCACHE)
+  {
+    mutt_buffer_printf(err, "Variable is not an hcache");
+    return NULL;
+  }
+
+  int index = (intptr_t) e->data;
+  if ((index < 0) || (index >= mutt_array_size(hcache_backends)))
+  {
+    mutt_buffer_printf(err, "Invalid hcache value: %d", index);
+    return NULL;
+  }
+
+  return hcache_backends[index];
 }
 
-static bool validator(const char *name, int type, intptr_t value, struct Buffer *err)
+static bool hc_validator(const char *name, int type, intptr_t value, struct Buffer *err)
 {
   // validate an hcache string
   return true;
 }
 
-static bool destructor(struct HashElem *e, struct Buffer *err)
+static bool hc_destructor(struct HashElem *e, struct Buffer *err)
 {
-  // destroy an hcache string
+  /* We only store an int, so no action is required */
   return true;
 }
 
+
 bool hcache_init(void)
 {
-  struct ConfigSetType cst = { string_set, string_get, validator, destructor };
+  struct ConfigSetType cst = { hc_string_set, hc_string_get, hc_validator, hc_destructor };
 
   cs_register_type("hcache", DT_HCACHE, &cst);
 
   return true;
 }
-
